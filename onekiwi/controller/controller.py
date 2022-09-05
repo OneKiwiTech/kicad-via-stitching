71b0e73e4d611238682a719a5f827295ed353f73
Van Son <thanh.duong.vs@gmail.com>
2022-09-05 11:36:02 +0700

    add via

--- onekiwi/controller/controller.py
+++ onekiwi/controller/controller.py
@@ -7,6 +7,7 @@ import sys
 import logging
 import logging.config
 
+# https://github.com/weirdgyn/viastitching/blob/master/viastitching_dialog.py
 class Controller:
     def __init__(self, board):
         self.view = ViaStitchingView()
@@ -46,7 +47,7 @@ class Controller:
         pcbnew.Refresh()
 
     def OnButtonApply(self, event):
-        pass
+        self.FillupArea()
     
     def OnButtonClear(self, event):
         self.area.ClearBrightened()
@@ -122,3 +123,57 @@ class Controller:
         root.addHandler(handler2)
         return logging.getLogger(__name__)
     
+    def FillupArea(self):
+        '''Fills selected area with vias.'''        
+
+        drillsize = self.model.vias[0].m_Drill
+        viasize = self.model.vias[0].m_Diameter
+        step_x = 2000000
+        step_y = 2000000
+        #clearance = self.FromUserUnit(float(self.m_txtClearance.GetValue()))
+
+        index = self.view.choiceArea.GetSelection()
+        area = self.model.areas[index]
+
+        bbox = area.GetBoundingBox()
+        top = bbox.GetTop()
+        bottom = bbox.GetBottom()
+        right = bbox.GetRight()
+        left = bbox.GetLeft()
+        
+        #index = self.view.choiceArea.GetSelection()
+        #area = self.model.areas[index]
+        netname = str(area.GetNetname())
+        netcode = self.board.GetNetcodeFromNetname(netname)
+        #commit = pcbnew.COMMIT()
+        viacount = 0
+        x = left
+
+        #Cycle trough area bounding box checking and implanting vias
+        layer = area.GetLayer()
+        while x <= right:
+            y = top
+            while y <= bottom:
+                p = pcbnew.wxPoint(x,y)
+                if area.HitTestFilledArea(layer, p, 0):
+                    via = pcbnew.PCB_VIA(self.board)
+                    via.SetPosition(p)
+                    via.SetLayer(layer)
+                    via.SetNetCode(netcode)
+                    via.SetDrill(drillsize)
+                    via.SetWidth(viasize)
+                    #via.SetTimeStamp(__timecode__)
+                    self.board.Add(via)
+                    viacount +=1
+                    """
+                    if not self.CheckOverlap(via):
+                        #Check clearance only if clearance value differs from 0 (disabled)
+                        if (clearance == 0) or self.CheckClearance(p, self.area, clearance):
+                            self.board.Add(via)
+                            #commit.Add(via)
+                            viacount +=1
+                            """
+                y += step_y
+            x += step_x
+
+        pcbnew.Refresh()
